73de885cac50b6e7b2395ee86eb3b033d2f09d29
Evgeniyatorva <Evgeniyatorva@mail.ru>
2017-10-03 04:01:12 +0300

    advocatus

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//slider
+
+$('.slide').slick({
+	arrows: false,
+	dots: true
+});
+
+//tabs
+
+$( function() {
+	$( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+	$( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+});
+
+$('.ui-tabs-vertical .ui-tabs-nav li a').click( function (){
+	$('.ui-tabs-vertical .ui-tabs-nav li a:after').css({"display":"none"});
+});
+
+//menu
